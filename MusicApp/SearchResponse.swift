72b429f5eb191786bfee6dfa3937a5fe01451d3a
Andrey <akart.dev@gmail.com>
2022-08-25 17:40:38 +0600

    response data

---
+++ MusicApp/SearchResponse.swift
@@ -0,0 +1,20 @@
+//
+//  SearchResponse.swift
+//  MusicApp
+//
+//  Created by Andrey Kim on 25.08.2022.
+//
+
+import Foundation
+
+struct SearchResponse: Decodable {
+    var resultCount: Int
+    var results: [Track]
+}
+
+struct Track: Decodable {
+    var trackName: String
+    var collectionName: String?
+    var artistName: String
+    var artworkUrl100: String?
+}
--- MusicApp/SearchViewController.swift
+++ MusicApp/SearchViewController.swift
@@ -16,13 +16,14 @@ struct TrackModel {
 
 class SearchViewController: UITableViewController {
     
+    private var timer: Timer?
+    
     private lazy var searchController: UISearchController = {
         let search = UISearchController(searchResultsController: nil)
         return search
     }()
     
-    let tracks = [TrackModel(trackName: "bad guy", artistName: "Billie Eilish"),
-                 TrackModel(trackName: "bury a friend", artistName: "Billie Eilish")]
+    var tracks = [Track]()
     
     
     override func viewDidLoad() {
@@ -56,17 +57,30 @@ class SearchViewController: UITableViewController {
 extension SearchViewController: UISearchBarDelegate {
     
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        let url = "https://itunes.apple.com/search?term=\(searchText)"
         
-        AF.request(url).responseData { (dataResponse) in
-            if let error = dataResponse.error {
-                print("Error received requesting data: \(error.localizedDescription)")
-                return
-            }
+        timer?.invalidate()
+        timer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: false, block: { _ in
+            let url = "https://itunes.apple.com/search"
+            let parametrs = ["term": "\(searchText)", "limit": "10"]
             
-            guard let data = dataResponse.data else { return }
-            let someString = String(data: data, encoding: .utf8)
-            print(someString ?? "")
-        }
+            AF.request(url, method: .get, parameters: parametrs, headers: nil).responseData { (dataResponse) in
+                if let error = dataResponse.error {
+                    print("Error received requesting data: \(error.localizedDescription)")
+                    return
+                }
+                
+                guard let data = dataResponse.data else { return }
+                let decoder = JSONDecoder()
+                do {
+                    let objects = try decoder.decode(SearchResponse.self, from: data)
+                    print("objects: ", objects)
+                    self.tracks = objects.results
+                    self.tableView.reloadData()
+                    
+                } catch let jsonError {
+                    print("Failed to decode JSON", jsonError)
+                }
+            }
+        })
     }
 }
